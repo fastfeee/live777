54a10fc630e14b4d931d9e75d8ff70279aa3ca2e
fastfeee <3307603357@qq.com>
2024-03-11 23:09:26 +0800

    test:modified e2e.sh->another try

--- e2e.sh
+++ e2e.sh
@@ -2,6 +2,8 @@
 
 TARGET_DIR="./target/release"
 
+cd $TARGET_DIR
+
 cat >stream.sdp <<EOF
 v=0
 m=video 5004 RTP/AVP 96
@@ -9,35 +11,6 @@ c=IN IP4 127.0.0.1
 a=rtpmap:96 VP8/90000
 EOF
 
-cat >out1.sh <<EOF
-#!/bin/bash
-${TARGET_DIR}/ffmpeg -re -f lavfi -i testsrc=size=640x480:rate=30 -vcodec libvpx -f rtp 'rtp://127.0.0.1:5003?pkt_size=1200' -c:v libvpx  -quality realtime output1.webm
-EOF
-
-chmod +x out1.sh
-
-cat >whip.sh <<EOF
-#!/bin/bash
-${TARGET_DIR}/whipinto -c vp8 -u http://localhost:7777/whip/777 --port 5003
-EOF
-
-chmod +x whip.sh
-chmod +x ${TARGET_DIR}/whipinto
-
-cat >whep.sh <<EOF
-#!/bin/bash
-${TARGET_DIR}/whepfrom -c vp8 -u http://localhost:7777/whep/777 -t localhost:5004
-EOF
-
-chmod +x whep.sh
-chmod +x ${TARGET_DIR}/whepfrom
-
-cat >out2.sh <<EOF
-#!/bin/bash
-${TARGET_DIR}/ffmpeg -protocol_whitelist rtp,file,udp -i stream.sdp -c:v copy -an output2.webm
-EOF
-
-chmod +x out2.sh
 
 cat >eval.sh <<EOF
 #!/bin/bash
@@ -49,27 +22,22 @@ chmod +x eval.sh
 ./test.sh \
     "${TARGET_DIR}/live777" \
     "sleep 2"  \
-    "./whip.sh" \
-    "./whep.sh" \
-    "./out2.sh" \
-    "./out1.sh" \
-# 等待5秒
-sleep 5
+    "${TARGET_DIR}/whipinto -c vp8 -u http://localhost:7777/whip/777 --port 5003" \
+    "${TARGET_DIR}/whepfrom -c vp8 -u http://localhost:7777/whep/777 -t localhost:5004" \
+    "ffmpeg -protocol_whitelist rtp,file,udp -i stream.sdp -c:v copy -an output2.webm" \
+    "timeout 5 ffmpeg -re -f lavfi -i testsrc=size=640x480:rate=30 -vcodec libvpx -f rtp 'rtp://127.0.0.1:5003?pkt_size=1200' -c:v libvpx  -quality realtime output1.webm"
 
-# 中断 push.sh 进程
-if [ -n "$(ps -p ${PID[5]} -o pid=)" ]; then
-  echo "Stopping out1.sh pid ${PID[5]}"
-  kill ${PID[5]}
+
+# 等待./test.sh执行完成后执行./eval.sh
+if [ -f "output1.webm" ] && [ -f "output2.webm" ]; then
+    ./eval.sh
+else
+    echo "Error: output1.webm or output2.webm not generated."
 fi
 
-# 继续./eval.sh
-./eval.sh 
- 
 rm stream.sdp
-rm whip.sh
-rm whep.sh
 rm eval.sh
-rm out1.sh
-rm out2.sh
+
+
 
 
--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 ARGC=$#
 
 if [[ ${ARGC} == 0 ]]; then
-  echo -e "\033[44;37;5m INPUT \033[0m ./multirun.sh 'python3 -m http.server 8001' 'python3 -m http.server 8002' 'sleep 10' '<...>'"
+  echo -e "\033[44;37;5m INPUT \033[0m ./test.sh 'python3 -m http.server 8001' 'python3 -m http.server 8002' 'sleep 10' '<...>'"
   exit
 fi
 
 echo -e "\033[44;37;5m RUNNING \033[0m  ${ARGC} COMMANDS"
 
 for id in $(seq 1 $ARGC); do
-  $(eval echo '$'{$id}) &
+  ${TARGET_DIR}/$(eval echo '$'{$id}) &
   PID[id]=$!
 
-  echo "RUN ${id} pid ${PID[id]} : $(eval echo '$'{$id})"
+  echo "RUN ${id} pid ${PID[id]} : ${TARGET_DIR}/$(eval echo '$'{$id})"
 done
 
