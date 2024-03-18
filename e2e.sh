3778d8e564c15d120326affc2b08421038e7544c
fastfeee <3307603357@qq.com>
2024-03-18 22:13:28 +0800

    test:e2e.sh->check psnr

--- e2e.sh
+++ e2e.sh
@@ -33,14 +33,14 @@ sleep 15
 pkill live777
 pkill whipinto
 pkill whepfrom
-ffmpeg -i output1.webm -pix_fmt yuv420p output1.yuv
-ffmpeg -i output2.webm -pix_fmt yuv420p output2.yuv
+# ffmpeg -i output1.webm -pix_fmt yuv420p output1.yuv
+# ffmpeg -i output2.webm -pix_fmt yuv420p output2.yuv
 
 # 检查是否成功创建output2.yuv文件
-if [ -f "output1.yuv" ] && [ -f "output2.yuv" ]; then
-    echo "output1.yuv 和 output2.yuv 文件均已成功生成"
-    echo "output1.yuv 文件路径为: $(readlink -f output1.yuv)"
-    echo "output2.yuv 文件路径为: $(readlink -f output2.yuv)"
+if [ -f "output1.webm" ] && [ -f "output2.webm" ]; then
+    echo "output1.webm 和 output2.webm 文件均已成功生成"
+    echo "output1.webm 文件路径为: $(readlink -f output1.webm)"
+    echo "output2.webm 文件路径为: $(readlink -f output2.webm)"
 
     # 如果两个文件都生成成功，可以继续执行后续的命令
     # ...
