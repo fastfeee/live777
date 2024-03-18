c02beadf07b12e3f0c182ad45162ba5f94ceb52a
fastfeee <3307603357@qq.com>
2024-03-18 17:15:16 +0800

    test:modified e2e.sh

--- e2e.sh
+++ e2e.sh
@@ -18,26 +18,24 @@ else
     echo "Error: stream.sdp 文件未生成"
 fi
 
-
-gnome-terminal -- bash -c "./live777; exec bash"
-gnome-terminal -- bash -c "./whipinto -c vp8 -u http://localhost:7777/whip/777 --port 5003;exec bash"
-gnome-terminal -- bash -c "./whepfrom -c vp8 -u http://localhost:7777/whep/777 -t localhost:5004;exec bash"
-sleep 5
-gnome-terminal -- bash -c "ffmpeg -protocol_whitelist rtp,file,udp -i stream.sdp -c:v copy -an output2.webm"
-sleep 5
-gnome-terminal -- bash -c "ffmpeg -re -f lavfi -i testsrc=size=640x480:rate=30 -vcodec libvpx -f rtp 'rtp://127.0.0.1:5003?pkt_size=1200' -c:v libvpx  -quality realtime output1.webm"
+# 定义命令列表
+./live777 &
+sleep 2
+./whipinto -c vp8 -u http://localhost:7777/whip/777 --port 5003 & 
+sleep 2
+./whepfrom -c vp8 -u http://localhost:7777/whep/777 -t localhost:5004 &
+sleep 2
+ffmpeg -protocol_whitelist rtp,file,udp -i stream.sdp -c:v copy -an output2.webm &
+ffmpeg -re -f lavfi -i testsrc=size=640x480:rate=30 -vcodec libvpx -f rtp 'rtp://127.0.0.1:5003?pkt_size=1200' -c:v libvpx  -quality realtime output1.webm  &
 sleep 10
-
 pgrep -f "ffmpeg -re -f lavfi -i testsrc=size=640x480:rate=30" | xargs kill
-sleep 25
+sleep 15
 pkill live777
 pkill whipinto
 pkill whepfrom
-sleep 5 
 ffmpeg -i output1.webm -pix_fmt yuv420p output1.yuv
 ffmpeg -i output2.webm -pix_fmt yuv420p output2.yuv
 rm stream.sdp
-
 
 
 
